b02fd6b1b79de4f15ef71d39a8d280101ed35612
Pornpan Thongdee <pornpanthongdee02@gmail.com>
2022-06-07 10:51:44 +0800

    heroku

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
-img_file_buffer = st.camera_input("Take a picture")
+img_file_buffer = st.camera_input("Take a picture!")
 
 if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
