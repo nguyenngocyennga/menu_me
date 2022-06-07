620c7726b43b0e8dea44d2c074f470ff457f09f8
rubenstuiver <rwstuiver@gmail.com>
2022-06-07 15:04:37 +0800

    minor changes, included the function for translation api but needs to be adjusted for new preprocessor

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ import string
 img_file_buffer = st.camera_input("Take a picture!")
 
 
+###############################
+###### Google Vision API ######
+###############################
 def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
@@ -33,6 +36,10 @@ def detect_text(path):
     return '\n"{}"'.format(texts.description)
 
 
+
+###############################
+###### Text preprocessor ######
+###############################
 chars_to_remove = ''
 chars_to_remove += string.punctuation
 chars_to_remove += string.digits
@@ -50,6 +57,36 @@ def strip(raw_text):
     print()
     print(clean_entries)
     return clean_entries
+
+###############################
+######   Translate API   ######
+###############################
+target='en'
+def translate_text(target, text):
+    """Translates text into the target language.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    import six
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    if isinstance(text, six.binary_type):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target)
+
+    print(u"Text: {}".format(result["input"]))
+    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+
+
+
 
 if img_file_buffer is not None:
     # To read image file buffer as a PIL Image:
