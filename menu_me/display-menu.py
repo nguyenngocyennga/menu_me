ff29fd08e74806a8f1567fa93e404be67ffea14c
Nga Nguyen <nganguyen.ny@gmail.com>
2022-06-09 09:45:35 +0800

    new front-end and display function

--- menu_me/display-menu.py
+++ menu_me/display-menu.py
@@ -11,29 +11,7 @@ import os
 from google.oauth2 import service_account
 import json
 from dotenv import load_dotenv, find_dotenv
-
-
-##################################
-####   Streamlit home page    ####
-##################################
-env_path = find_dotenv()
-load_dotenv(env_path)
-CREDENTIALS_JSON_GOOGLE_CLOUD = os.getenv('CREDENTIALS_JSON_GOOGLE_CLOUD')
-
-
-img_file_buffer = st.camera_input("Take a picture!")
-
-if img_file_buffer is not None:
-    img = Image.open(img_file_buffer)
-    rgb_im = img.convert("RGB")
-    rgb_im.save("img.jpg")
-    credentials = service_account.Credentials.from_service_account_info(json.loads(CREDENTIALS_JSON_GOOGLE_CLOUD))
-    client = storage.Client(credentials=credentials, project='menu-me-352703')
-    bucket = client.get_bucket('menu_me_bucket')
-    blob = bucket.blob("img.jpg")
-    blob.upload_from_filename("img.jpg")
-    st.write('upload success')
-
+from api.api import api_function
 
 ###############################
 ######## Display menu #########
@@ -51,3 +29,31 @@ def display_menu(df):
                 <p>{row['translated_name']}</p>
             </div>
             </div>''', unsafe_allow_html=True)
+
+
+##################################
+####   Streamlit home page    ####
+##################################
+env_path = find_dotenv()
+load_dotenv(env_path)
+CREDENTIALS_JSON_GOOGLE_CLOUD = os.getenv('CREDENTIALS_JSON_GOOGLE_CLOUD')
+
+
+# img_file_buffer = st.camera_input("Take a picture!")
+
+# if img_file_buffer is not None:
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    rgb_im = img.convert("RGB")
+    rgb_im.save("img.jpg")
+    credentials = service_account.Credentials.from_service_account_info(json.loads(CREDENTIALS_JSON_GOOGLE_CLOUD))
+    client = storage.Client(credentials=credentials, project='menu-me-352703')
+    bucket = client.get_bucket('menu_me_bucket')
+    blob = bucket.blob("img.jpg")
+    blob.upload_from_filename("img.jpg")
+    st.write('Photo is uploaded 🥳')
+    st.write('Your menu is coming soon... 🌮 🌯 🥙')
+    df = api_function()
+    display_menu(df)
+
